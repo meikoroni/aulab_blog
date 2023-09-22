5f931c6b78e62a5624c14e6fdabfd46737a2373a
veronica lopez <meikoroni@hotmail.com>
2023-09-22 21:12:53 +0200

    settings

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+<x-layout>
+    <div class="container-fluid p-5 bg-info text-center text-white">
+      <div class="row justify-content-center">
+        <h1 class="display-1">
+            Aulab Post
+        </h1>
+      </div>
+    </div>
+  </x-layout>
