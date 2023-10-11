2caea4b0f043df8efb7aaef6a32b7f10fd4c4d1a
veronica lopez <meikoroni@hotmail.com>
2023-10-11 19:00:25 +0200

    modelo_user

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
     ];
 
     public function articles(){
-       return $this -> hasMany(Artaulñicle::class);
+       return $this -> hasMany(Article::class);
     }
 }
