4b871ed534219d544fefad75640e988f5f88ed06
Bea Serrano <beaserranolucio@gmail.com>
2023-10-18 20:49:18 +0200

    tabla arreglada

--- database/migrations/2023_10_16_185102_add_roles_to_users_table.php
+++ database/migrations/2023_10_16_185102_add_roles_to_users_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
